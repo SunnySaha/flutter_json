de274f13bbbe0485fca53bd047428f61f998716e
sunny_git <scsahasunny@gmail.com>
2019-04-06 16:03:11 +0600

    json Horizontal

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget{
 
     return MaterialApp(
       home: MyList(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }//end of StatelessWidget
@@ -29,7 +30,7 @@ class _MyJsonList extends State<MyList>{
 
   Future<List<userList>> _getUserList() async{
 
-    String url = 'https://www.json-generator.com/api/json/get/cfwZmvEBbC?indent=2';
+    String url = "https://www.json-generator.com/api/json/get/cfwZmvEBbC?indent=2";
 
     var data= await http.get(url);//collect data from url
 
@@ -38,7 +39,7 @@ class _MyJsonList extends State<MyList>{
     List<userList> User =[];
 
     for(var i in jsonDataObject){
-      userList userlist = userList(i["index"],i["name"], i["about"],i["email"],i["picture"] );
+      userList userlist = userList(i["index"],i["name"], i["about"],i["email"], i["picture"] );
       User.add(userlist); //adding data in list
     }//end of for loop
     return User;
@@ -49,46 +50,81 @@ class _MyJsonList extends State<MyList>{
   Widget build(BuildContext context) {
 
     return new Scaffold(
+
       appBar: new AppBar(
         title: new Text('Json List'),
+
       ),
       body: Container(
+        height: 200.0,
         child: FutureBuilder(
           future: _getUserList(),
           builder: (BuildContext context, AsyncSnapshot snapshot){
             return ListView.builder(
+              scrollDirection:Axis.horizontal,
               itemCount: snapshot.data.length,
               itemBuilder: (BuildContext context, int index) {
                 if (snapshot.data == null) {
+
                   return Container(
                     child: Center(
-                        child: Text('Data Asche!!!')
-                    ),
+                  child: Text('Data Reloaded'),
+                  ),
+
                   );
-                } else {
-                  return ListTile(
-                    leading:CircleAvatar(
-                      backgroundImage: NetworkImage(
-                        snapshot.data[index].picture
-                      ),
-                    ),
-                    title: Text(snapshot.data[index].name),
-                    subtitle: Text(snapshot.data[index].email),
-                  );
+    }else{          return Container(
+                              width: 160.0,
+
+
+                            child: Card(
+
+
+                              child: Wrap(
+                                children: <Widget>[
+                                  Image.network(snapshot.data[index].picture),
+                                  Text(snapshot.data[index].name),
+                                  Text(snapshot.data[index].email)
+                                ],
+                              ),
+      ),
+    );
                 }
               }
-                 );
+              );
+    
+          
 
           },//end of builder
         ),
       ),
+
     );
   }//end of code of WidgetBuild
 
 }//end of code of State
 
+class userDetails extends StatelessWidget{
+
+  userList user;
+  userDetails(this.user);
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(user.name),
+      ),
+      body: Center(
+        child: Text(user.email),
+      ),
+    );
+  }
+  
+}
+
 class userList{
+
   final int index;
-  final String name, about, email, picture;
-  userList(this.index, this.name, this.about, this.email, this.picture);
+  final String  name, about, email, picture;
+  userList( this.index, this.name, this.about, this.email, this.picture);
 }
